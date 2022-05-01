c89e293c0ddc194ba2581c97e943b8f0ccc87c15
lcobell <lcobell@gmail.com>
2022-05-01 16:25:34 -0500

    attempt at adding suspense, Suspense is not defined, imported Suspense then error messages saying code in Gallery.js is not a function, Gallery.js code was copy and paste

--- src/App.js
+++ src/App.js
@@ -1,61 +1,93 @@
 import "./App.css";
-import { useEffect, useState, Fragment } from "react";
+import { useEffect, useState, Fragment, suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Gallery from "./components/Gallery";
 import Searchbar from "./components/Searchbar";
 import AlbumView from "./components/AlbumView";
 import ArtistView from "./components/ArtistView";
+import Spinner from "./components/Spinner";
+import { createResource as fetchData } from "./helper";
+import React, { lazy, Suspense } from "react";
 
 // import { DataContext } from './context/DataContext'
 
 function App() {
-  let [search, setSearch] = useState("");
+  let [searchTerm, setSearchTerm] = useState("");
   let [message, setMessage] = useState("Search for Music!");
-  let [data, setData] = useState([]);
+  //   let [data, setData] = useState([]); (commented out to try out suspense and see whats happening in dev tools)
+  let [data, setData] = useState(null);
 
   const API_URL = "https://itunes.apple.com/search?term=";
-
+  // added for suspense
   useEffect(() => {
-    if (search) {
-      const fetchData = async () => {
-        document.title = `${search} Music`;
-        const response = await fetch(API_URL + search);
-        const resData = await response.json();
-        if (resData.results.length > 0) {
-          setData(resData.results);
-        } else {
-          setMessage("Not Found");
-        }
-      };
-      fetchData();
+    if (searchTerm) {
+      document.title = `${searchTerm}Music`;
+      console.log(fetchData(searchTerm));
+      setData(fetchData(searchTerm));
     }
-  }, [search]);
+  }, [searchTerm]);
+
+  //removed for suspense
+  //   useEffect(() => {
+  //     if (search) {
+  //       const fetchData = async () => {
+  //         document.title = `${search} Music`;
+  //         const response = await fetch(API_URL + search);
+  //         const resData = await response.json();
+  //         if (resData.results.length > 0) {
+  //           setData(resData.results);
+  //         } else {
+  //           setMessage("Not Found");
+  //         }
+  //       };
+  //       fetchData();
+  //     }
+  //   }, [search]);
 
   const handleSearch = (e, term) => {
     e.preventDefault();
-    setSearch(term);
+    setSearchTerm(term);
+  };
+
+  const renderGallery = () => {
+    if (data) {
+      return (
+        <Suspense fallback={<Spinner />}>
+          <Gallery data={data} />
+        </Suspense>
+      );
+    }
   };
 
   return (
-    <div>
+    <div className="App">
+      <Searchbar handleSearch={handleSearch} />
       {message}
-      <Router>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <Fragment>
-                <Searchbar handleSearch={handleSearch} />
-                <Gallery data={data} />
-              </Fragment>
-            }
-          />
-          <Route path="/album/:id" element={<AlbumView />} />
-          <Route path="/artist/:id" element={<ArtistView />} />
-        </Routes>
-      </Router>
+      {renderGallery()}
     </div>
   );
 }
+
+//   return (
+//     <div>
+//       {message}
+//       <Router>
+//         <Routes>
+//           <Route
+//             path="/"
+//             element={
+//               <Fragment>
+//                 <Searchbar handleSearch={handleSearch} />
+//                 <Gallery data={data} />
+//               </Fragment>
+//             }
+//           />
+//           <Route path="/album/:id" element={<AlbumView />} />
+//           <Route path="/artist/:id" element={<ArtistView />} />
+//         </Routes>
+//       </Router>
+//     </div>
+//   );
+// }
 
 export default App;
